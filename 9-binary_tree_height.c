f6e79613b2cf568657f06e18c4f173612ce234dc
AbduHany <abdu.hany@gmail.com>
2024-03-26 17:55:10 +0200

    Modify 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,7 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height = 0;
-	binary_tree_t *cursor = (binary_tree_t*)tree;
+	binary_tree_t *cursor = (binary_tree_t *)tree;
 
 	if (tree == NULL)
 		return (0);
